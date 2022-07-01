a30bac3790e6f155ba7a5784e5dcd9340461b220
Lauren N Massey <laurennmassey@Laurens-MacBook-Pro.local>
2022-07-01 13:35:11 -0700

    html, head, title, meta tags to change browser tab

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
+    <Html :lang="dynamic > 50 ? 'en-GB' : 'en-US'">
+      <Head>
+        <Title>Restuarant Site</Title>
+        <Meta name="description" content="My page is awesome socks!"></Meta>
+        <Link
+          rel="preload"
+          href="https://www.iconpacks.net/icons/2/free-sad-face-icon-2691-thumb.png"
+          as="script"
+        />
+      </Head>
+    </Html>
     <div class="container">
       <h2>Welcome to Restaurants</h2>
       <NuxtLink to="/restaurants">Go to restaurants</NuxtLink>
--- pages/restaurants/index.vue
+++ pages/restaurants/index.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
+    <Html :lang="dynamic > 50 ? 'en-GB' : 'en-US'">
+      <Head>
+        <Title>Top 50 Restaurants</Title>
+        <Meta name="description" content="My page is awesome socks!"></Meta>
+        <Link
+          rel="preload"
+          href="https://www.iconpacks.net/icons/2/free-sad-face-icon-2691-thumb.png"
+          as="script"
+        />
+      </Head>
+    </Html>
     <NuxtLayout name="custom">
       <div class="table">
         <RestaurantTable />
